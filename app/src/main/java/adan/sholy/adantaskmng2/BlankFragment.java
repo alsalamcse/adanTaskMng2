4c657b2edc8e3e4fc97d793579ffb91865eb1c34
Othman <otma.ctos.asslan@hotmail.com>
2019-10-24 14:06:27 +0300

    ljuyf

---
+++ app/src/main/java/adan/sholy/adantaskmng2/BlankFragment.java
@@ -0,0 +1,43 @@
+package adan.sholy.adantaskmng2;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+    public void readTasksFrromFirebase()
+    {
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        String uid = auth.getUid();
+        DatabaseReference reference = database.getReference();
+
+    }
+
+}
--- app/src/main/java/adan/sholy/adantaskmng2/MainTasksActivity.java
+++ app/src/main/java/adan/sholy/adantaskmng2/MainTasksActivity.java
@@ -1,5 +1,6 @@
 package adan.sholy.adantaskmng2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,6 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-import com.example.adantaskmanager.ui.main.SectionsPagerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -17,13 +17,13 @@ public class MainTasksActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_tasks);
+        setContentView(R.layout.fragment_main_tasks);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        @SuppressLint("WrongViewCast") FloatingActionButton fab = findViewById(R.id.firstname);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
