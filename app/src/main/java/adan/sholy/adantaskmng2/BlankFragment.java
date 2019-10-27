f7322f8dfe91cce7ffe867253e42fbe10d374173
Othman <otma.ctos.asslan@hotmail.com>
2019-10-27 12:19:38 +0200

    ljuyf

--- app/src/main/java/adan/sholy/adantaskmng2/BlankFragment.java
+++ app/src/main/java/adan/sholy/adantaskmng2/BlankFragment.java
@@ -3,15 +3,20 @@ package adan.sholy.adantaskmng2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -37,7 +42,22 @@ public class BlankFragment extends Fragment {
         FirebaseAuth auth=FirebaseAuth.getInstance();
         String uid = auth.getUid();
         DatabaseReference reference = database.getReference();
+        reference.child("task").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) 
+            {
+                for (DataSnapshot d:dataSnapshot.getChildren())
+                {
+                    MyTask t=d.getValue(MyTask.class);
+                    Log.d("MYTASK",t.toString());
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
